0f95032bdebdf895f958e4ac7efededda860c008
beyzakoroglu <58954994+beyzakoroglu@users.noreply.github.com>
2024-06-17 00:44:12 +0300

    animal spawner added

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+
+    public float topBound = 30.0f;
+    public float lowerBound = -10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z > 50) 
+        {
+            Destroy(gameObject);
+        } else if(transform.position.z < -10) {
+            Destroy(gameObject);
+        }
+
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     private float range = 11.0f;
+
+    public GameObject projectilePrefab;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +24,19 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(range, transform.position.y, transform.position.z);
         }
+
         if(transform.position.x < -range)
         {
             transform.position = new Vector3(-range, transform.position.y, transform.position.z);
         }
 
-
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    
     }
 }
