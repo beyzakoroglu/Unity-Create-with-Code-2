6af51e1558ca28ec3364e252eb8c459870b343ea
beyzakoroglu <58954994+beyzakoroglu@users.noreply.github.com>
2024-06-17 15:07:05 +0300

    dog spawn cooldown added

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float spawnCooldown = 3f; // Spawn işlemi için gerekli minimum bekleme süresi
+    private float lastSpawnTime; // Son köpek yaratma zamanını takip eder
+
+
+    void Start()
+    {
+        lastSpawnTime = -spawnCooldown; // Oyunun başında köpek yaratılabilmesi için başlangıç zamanı
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time >= lastSpawnTime + spawnCooldown)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            lastSpawnTime = Time.time; // Son spawn zamanını güncelle
         }
     }
 }
